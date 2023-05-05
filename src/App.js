de4278170ac14061440326c803ac719a67f029f2
Rumena Staneva <rumenastaneva2106@gmail.com>
2023-05-05 14:48:39 +0300

    added tailwind css to the project

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
+import './index.css';
 
 function App() {
     return (
         <div>
-            <div>Personal digital assistance</div>
+
+            <div class="text-5xl font-extrabold ... text-center">
+                <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 p-4">Personal digital assistance</h1>
+            </div>
 
             <div className="cards">
                 <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
